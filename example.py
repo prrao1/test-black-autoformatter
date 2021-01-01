0f3b2944bbcf4b52f3c08f79d3fc605317526f6b
prrao1 <prashanth@askrose.com>
2021-01-01 12:32:22 -0500

    test query formatting with black

--- example.py
+++ example.py
@@ -6,5 +6,12 @@ print(x == y)
 # Test dict
 label_map = {"anxiety": 0, "depression": 1, "positive_mood": 2, "negative_mood": 3}
 
-# Test syntax error
-print("Incomplete statement")
+# Example SQL queries
+selection_query = (
+    "SELECT JID, Journal FROM `journal` "
+    "WHERE status = %(unique_timestamp)s "
+)
+# Example SQL queries
+insertion_query = (
+    "INSERT IGNORE INTO `journal_analysis` (JID, AnalysisValue, Flag, Indicators) VALUES (%(JID)s, %(AnalysisValue)s, %(Flag)s, %(Indicators)s) "
+)
