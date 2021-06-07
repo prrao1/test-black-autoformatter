396bbd2113acbcc11e584e1358137ef335d25ab8
prrao87 <prashanth@rosehealth.com>
2021-06-07 18:01:19 -0400

    Test action

--- example.py
+++ example.py
@@ -6,9 +6,7 @@ print(x == y)
 # Test dict
 label_map = {"anxiety": 0, "depression": 1, "positive_mood": 2, "negative_mood": 3}
 # Example SQL queries
-selection_query = (
-    "SELECT JID, Journal FROM `journal` " "WHERE status = %(unique_timestamp)s "
-)
+selection_query = ("SELECT JID, Journal FROM `journal` " "WHERE status = %(unique_timestamp)s ")
 
 # Example SQL queries
 insertion_query = "INSERT IGNORE INTO `journal_analysis` (JID, AnalysisValue, Flag, Indicators) VALUES (%(JID)s, %(AnalysisValue)s, %(Flag)s, %(Indicators)s) "
