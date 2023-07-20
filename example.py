f77116c422e1671a0198f44fbb74b9ac2774413a
prrao87 <prrao87@gmail.com>
2023-07-20 15:19:40 -0400

    Update example

--- example.py
+++ example.py
@@ -1,10 +1,14 @@
+import os
+import math
+
+
 # Example variable assignment and comparison
 x = 12
 y = 1
 print(x == y)
 
 # Test dict
-label_map = {"anxiety": 0, "depression": 1, "positive_mood": 2, "negative_mood": 3}
+label_map = {"happy": 0, "sad": 1, "angry": 2, "neutral": 3}
 # Example SQL queries
 selection_query = (
     "SELECT JID, Journal FROM `journal` " "WHERE status = %(unique_timestamp)s "
@@ -16,3 +20,9 @@ insertion_query = "INSERT IGNORE INTO `journal_analysis` (JID, AnalysisValue, Fl
 a = 1
 b = 2
 print(a == b)
+
+c = math.pi * 2
+
+print(f"2pi = {c:.5f}")
+
+print(os.getcwd())
